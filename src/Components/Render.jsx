97b0bcecc081be2129be96f6a4eab107c08dbba5
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-06-09 02:10:25 +0530

    update

--- src/Components/Render.jsx
+++ src/Components/Render.jsx
@@ -100,7 +100,7 @@ function addComponent(e) {
 useEffect(()=>{
 trialFetch(urlLink.slice(32,fl));
 // normalFetch(urlLink.slice(22,fl));   //(for localhost)
-normalFetch(urlLink.slice(41,fl));
+normalFetch(urlLink.slice(32,fl));
 },[])
   return (
     <div className='dataContent'>
