de0dd23c454988eca905b5a81e454e6b54a8e114
dreadarceus <dreadarceus@gmail.com>
2022-06-21 19:28:20 +0530

    Search Bar on Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,30 +2,38 @@ import React from "react";
 import "./components.css";
 import { useNavigate } from "react-router-dom";
 
-const Home = ({ channels }) => {
+const Home = ({ channels, getSearchBar, getSearchResults, searchResults }) => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ display: "flex" }}>
-      <div className="Categories">
-        <h1>Categories</h1>
-        <div>
-          <div className="routeTiles">
-            {channels.map((element) => {
-              return (
-                <div className="card">
-                  <div className="card-body">
-                    <div onClick={() => navigate("/" + element.name)}>
-                      #{element.name}
+    <>
+      {/*Style this to be similar to render page (right side of "Categories") */}
+      <div className="home-search">{getSearchBar()}</div>
+
+      <div style={{ display: "flex" }}>
+        <div className="Categories">
+          <h1>Categories</h1>
+          <div>
+            <div className="routeTiles">
+              {channels.map((element) => {
+                return (
+                  <div className="card">
+                    <div className="card-body">
+                      <div onClick={() => navigate("/" + element.name)}>
+                        #{element.name}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {/*Style this to be at the right of the categories list and make the categories be like the render message list (height overflow goes to scroll)*/}
+      {searchResults ? getSearchResults(navigate) : ""}
+    </>
   );
 };
 
