6b7982fd28b931d92fffe78e2d7e32335abfe5f5
dreadarceus <dreadarceus@gmail.com>
2022-06-27 10:11:26 +0530

    Temporary Home-Search styless

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,32 +7,36 @@ const Home = ({ channels, getSearchBar, getSearchResults, searchResults }) => {
 
   return (
     <>
-      {/*Style this to be similar to render page (right side of "Categories") */}
       <div className="home-search">{getSearchBar()}</div>
 
-      <div style={{ display: "flex" }}>
-        <div className="Categories">
-          <h1>Categories</h1>
-          <div>
-            <div className="routeTiles">
-              {channels.map((element) => {
-                return (
-                  <div className="card">
-                    <div className="card-body">
-                      <div onClick={() => navigate("/" + element.name)}>
-                        #{element.name}
+      {searchResults ? (
+        <div
+          style={{ marginTop: "50px", marginLeft: "30px", marginRight: "30px" }}
+        >
+          {getSearchResults(navigate)}
+        </div>
+      ) : (
+        <div style={{ display: "flex" }}>
+          <div className="Categories">
+            <h1>Categories</h1>
+            <div>
+              <div className="routeTiles">
+                {channels.map((element) => {
+                  return (
+                    <div className="card">
+                      <div className="card-body">
+                        <div onClick={() => navigate("/" + element.name)}>
+                          #{element.name}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      {/*Style this to be at the right of the categories list and make the categories be like the render message list (height overflow goes to scroll)*/}
-      {searchResults ? getSearchResults(navigate) : ""}
+      )}
     </>
   );
 };
